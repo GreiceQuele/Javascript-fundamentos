cda4a6816371d552506073ad63b4c64c9dec1e16
Quele <greice.caetano@gmail.com>
2023-04-18 13:54:12 -0300

    acrescentei novas formas de transformar um valor em string

--- README.md
+++
@@ -1,3 +0,0 @@
-# Javascript-fundamentos
-Exemplos e projetos realizados no curso da Alura JavaScript: tipos, varáveis e funções
-
--- type-string.js
+++ type-string.js
@@ -1,4 +1,4 @@
-const texto1 = "Olá, mundo!";
+/*const texto1 = "Olá, mundo!";
 const texto2 = 'Olá, mundo!';
 const senha = "senhaSuperSegura456!";
 const StringDeNumeros = "34567";
@@ -11,3 +11,9 @@ const meuNome = "Leonardo";
 console.log(citacao + meuNome)
 
 //template string OU template literal
+*/
+//let telefone = 12341234;
+//console.log("O telefone é " + String(telefone)); // teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+
+let telefone = 12341234;
+console.log("O telefone é " + telefone.toString()); // o .toString() é uma outra forma para  fazer essa conversão, que é mais parecida com outras linguagens de programação.
