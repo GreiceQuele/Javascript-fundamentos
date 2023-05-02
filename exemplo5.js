040cb92d41b2e59ce6b657ab6ace49cd8f9daf41
Quele <greice.caetano@gmail.com>
2023-05-02 16:55:47 -0300

    adcionando mais exemplos da aula

---
+++ exemplo5.js
@@ -0,0 +1,53 @@
+/*const num = 10;
+
+if (num === null) {
+ console.log('número não fornecido');
+}
+
+
+const num = 10;
+
+if (num !== null) {
+ console.log(`o número é ${num}`);
+}
+
+
+const num = 10;
+
+if (num) {
+    console.log(`o número é ${num}`);
+   }
+
+*/
+//const num = null;
+/*
+if (num === null) {
+ console.log('número não fornecido');
+}
+
+
+
+if (!num) {
+    console.log('número não fornecido');
+   }
+
+
+const num = 11;
+
+if (num > 10 || !num) {
+ console.log('número não válido');
+}
+
+*/
+
+const num = 15;
+
+if (num >= 0 && num <= 10) {
+  console.log('número entre zero e dez');
+} else if (num > 10 && num <= 20) {
+  console.log('número entre dez e vinte');
+} else if (num > 20 && num <= 30) {
+  console.log('número entre vinte e trinta');
+} else {
+  console.log('outro número');
+}
